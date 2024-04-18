50b1718e9a06cfa7e6cc1d284b484ae31a7ca973
bbnoir <bbnoir83@gmail.com>
2024-04-18 17:05:07 +0800

    feat: HW2 - add ignore and mod output

--- HW2/src/main.cpp
+++ HW2/src/main.cpp
@@ -7,9 +7,14 @@ int main()
     DataLoader* dl_train = new DataLoader("HW2_training.csv");
     DataLoader* dl_test = new DataLoader("HW2_testing.csv");
 
+    std::cout << "==== Generative Model ====" << std::endl;
     GenModel* gen_model = new GenModel(2, 4);
+    std::cout << std::endl;
+    std::cout << "---- Training ----" << std::endl;
     gen_model->Train(dl_train);
     gen_model->Test(dl_train);
+    std::cout << std::endl;
+    std::cout << "---- Testing ----" << std::endl;
     gen_model->Test(dl_test);
 
     return 0;
